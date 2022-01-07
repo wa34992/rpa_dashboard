e35f4a0af2b84bf8ecb80e78329f945ef05c0ca4
wa34992 <wa34992@gmail.com>
2022-01-07 19:46:26 +0500

    UPDATE: according to new requirements.

--- process.py
+++ process.py
@@ -1,9 +1,11 @@
+import time
 from datetime import timedelta
 import os
 from config import OUTPUT, create_directory, URL
 from RPA.Browser.Selenium import Selenium
 from RPA.Excel.Files import Files
 from RPA.PDF import PDF
+from config import AGENCY_NAME
 
 
 class DashBoard:
@@ -13,143 +15,132 @@ class DashBoard:
         self.files = Files()
         self.pdf = PDF()
         self.browser = Selenium()
+        self.urls = []
+        self.agencies_data = {}
+        self.agency_table = {}
+
+    def dive_in(self):
+        """
+        Navigate to itdashboard.org and click on Dive in
+        :return:
+        """
         self.browser.set_download_directory(OUTPUT)
         self.browser.open_available_browser(URL, maximized=True)
-        self.urls = []
-        self.agency_data = {}
-
-    def dive_in_agencies(self):
-
-        """
-        This function is used to click "Dive In" scrape name and Total FYI 2021 Spending and
-        add the data in Excel sheet
-        :return: None
-
-        """
-
-        agencies = []
-        amounts = []
-
-        self.browser.wait_until_page_contains('DIVE IN')
+        self.browser.wait_until_page_contains("Dive in")
         self.browser.find_element('//a[@aria-controls="home-dive-in"]').click()
-        self.browser.wait_until_page_contains_element("//div[@id='agency-tiles-container']")
+        self.browser.wait_until_page_contains_element('//div[@id="agency-tiles-container"]')
+
+    def scrap_agencies(self):
+        """
+        read names and amount of all the agencies that appear on the page after clicking on Dive in.
+        :return:
+        """
+        agencies = self.browser.find_elements('//div[@id="agency-tiles-container"]//span[@class="h4 w200"]')
+        amounts = self.browser.find_elements('//div[@id="agency-tiles-container"]//span[@class="h1 w900"]')
         while True:
-            agency_list = self.browser.find_elements('//span[@class="h4 w200"]')
-            amount_list = self.browser.find_elements('//span[@class=" h1 w900"]')
-            if len(agency_list) > 20:
-                break
-
-        for agency in agency_list:
-            if agency.text != "":
-                agencies.append(agency.text)
-        for amount in amount_list:
-            amounts.append(amount.text)
-        self.agency_data = {'Agencies': agencies, 'Amounts': amounts}
-
-    def agency_table_scrape(self, agency_name):
-        """
-
-        This function perform click operation on single agency and scroll down to
-        Individual Investment table scrape all the table and add the data to an Excel Sheet
-
-        :return: None
-
-        """
-
-        uii_list = []
-        bureau_list = []
-        investment_title_list = []
-        spending_list = []
-        type_list = []
-        cio_rating_list = []
-        no_of_projects_list = []
-        pdf_matched = []
-        self.browser.find_element(f'//span[contains(text(),"{agency_name}")]').click()
-        self.browser.wait_until_page_contains_element('//table[@id="investments-table-object"]', timeout=timedelta(seconds=20))
-        self.browser.select_from_list_by_value('//div[@class="dataTables_length"]//label//select', "-1")
-        row_range = self.browser.find_element('//div[@id="investments-table-object_info"]').text
-        row_range_int = int(row_range.split(' ')[5])
-        self.browser.wait_until_page_contains_element(f'//table[@id="investments-table-object"]//tbody//tr[{row_range_int}]',
-                                                      timeout=timedelta(seconds=20))
-        for i in range(1, row_range_int + 1):
-            uii = self.browser.find_element(f'//table[@id="investments-table-object"]//tbody//tr[{i}]//td[1]')
-            uii_list.append(uii.text)
-            bureau = self.browser.find_element(f'//table[@id="investments-table-object"]//tbody//tr[{i}]//td[2]')
-            bureau_list.append(bureau.text)
-            investment_title = self.browser.find_element(f'//table[@id="investments-table-object"]//tbody//tr[{i}]/td[3]')
-            investment_title_list.append(investment_title.text)
-            spending = self.browser.find_element(f'//table[@id="investments-table-object"]//tbody//tr[{i}]/td[4]')
-            spending_list.append(spending.text)
-            types = self.browser.find_element(f'//table[@id="investments-table-object"]//tbody//tr[{i}]/td[5]')
-            type_list.append(types.text)
-            cio_rating = self.browser.find_element(f'//table[@id="investments-table-object"]//tbody//tr[{i}]/td[6]')
-            cio_rating_list.append(cio_rating.text)
-            no_of_projects = self.browser.find_element(f'//table[@id="investments-table-object"]//tbody//tr[{i}]/td[7]')
-            no_of_projects_list.append(no_of_projects.text)
-
-            try:
-                url = self.browser.find_element(
-                    f'//*[@id="investments-table-object"]/tbody/tr[{i}]/td[1]').find_element_by_tag_name(
-                    "a").get_attribute("href")
-                self.pdf_download(url, uii_list[-1])
-                match = self.data_matching(uii_list[-1], investment_title_list[-1])
-                if match:
-                    pdf_matched.append("Matched")
-                # self.urls.append({'URL': url, 'Investment Title': investment_title_list, 'UII': uii_list})
-            except:
-                pdf_matched.append("Not Matched")
-
-        table_data = {"UII": uii_list, "Bureau": bureau_list, "Investment Title": investment_title_list,
-                      'Total FYI 2021 Spendings': spending_list, "Type": type_list, 'CIO Rating': cio_rating_list,
-                      '# of Projects': no_of_projects_list, "pdf matched": pdf_matched
-                      }
-
-        self.files.create_workbook(f'{OUTPUT}/data.xlsx')
-        self.files.rename_worksheet("Sheet", "Agencies")
-        self.files.append_rows_to_worksheet(self.agency_data, "Agencies", header=True)
-        self.files.create_worksheet('Individual Investment')
-        self.files.append_rows_to_worksheet(content=table_data, header=True)
-        self.files.save_workbook()
-        self.files.close_workbook()
-
-    def pdf_download(self, url, uii):
-        """
-        This function perform PDF downloading Operation in output folder
-        :return: None
-
-        """
-        self.browser.execute_javascript(f"window.open('{url}');")
-        tabs = self.browser.get_window_handles()
-        self.browser.switch_window(tabs[-1])
-        self.browser.wait_until_page_contains_element('//a[text()="Download Business Case PDF"]')
-        self.browser.find_element('//a[text()="Download Business Case PDF"]').click()
-        while True:
-            if os.path.exists(f"{OUTPUT}/{uii}.pdf"):
-                break
+            if len(amounts) == 0 or len(agencies) == 0:
+                time.sleep(3)
+                agencies = self.browser.find_elements('//div[@id="agency-tiles-container"]//span[@class="h4 w200"]')
+                amounts = self.browser.find_elements('//div[@id="agency-tiles-container"]//span[@class=" h1 w900"]')
             else:
-                pass
-        self.browser.close_window()
-        self.browser.switch_window(tabs[0])
+                break
+        agency_names = []
+        agency_amounts = []
+        for agency, amount in zip(agencies, amounts):
+            agency_names.append(agency.text)
+            agency_amounts.append(amount.text)
 
-    def data_matching(self, uii, investment):
+        self.agencies_data["Agency"] = agency_names
+        self.agencies_data["Amount"] = agency_amounts
+
+    def scrap_agency_table(self, agency):
+        """
+        scrap
+        :return:
+        """
+        pdf_links = []
+        pdf_index = []
+        self.browser.find_element(f"//span[text()='{agency}']").click()
+        self.browser.wait_until_page_contains_element("//table[@id='investments-table-object']",
+                                                      timeout=timedelta(seconds=10))
+        table_headers = self.get_headers()
+        for header in table_headers:
+            self.agency_table[header] = []
+        self.agency_table["PDF"] = []
+        self.browser.select_from_list_by_value("//select[@name='investments-table-object_length']", "-1")
+        total_rows = self.browser.find_element("//div[@class='dataTables_info']").text.split(" ")[-2]
+        self.browser.wait_until_page_contains_element(f"//table[@id='investments-table-object']//tr[{total_rows}]",
+                                                      timeout=timedelta(seconds=20))
+        rows = self.browser.find_elements(f"//table[@id='investments-table-object']//tbody//tr")
+        row_list = []
+        for count, row in enumerate(rows):
+            row_cells = row.find_elements_by_tag_name("td")
+            raw_data = []
+            for cell, header in zip(row_cells, table_headers):
+                try:
+                    link = cell.find_element_by_tag_name("a").get_attribute("href")
+                    pdf_links.append(link)
+                    pdf_index.append(count)
+                except:
+                    pass
+                self.agency_table[header].append(cell.text)
+            row_list.append(raw_data)
+        self.download_pdfs(pdf_links, pdf_index)
+        self.match_pdfs(pdf_index)
+        self.write_data_to_workbook()
+
+    def download_pdfs(self, pdf_links, pdf_index):
+        """
+        download pdfs where link is found in table
+        :param pdf_links: list of pdf links to download
+        :param pdf_index: index of pdf row in table
+        :return:
+        """
+        for link, index in zip(pdf_links, pdf_index):
+            self.browser.execute_javascript(f"window.open('{link}');")
+            tabs = self.browser.get_window_handles()
+            self.browser.switch_window(tabs[-1])
+            self.browser.wait_until_page_contains_element('//a[text()="Download Business Case PDF"]',
+                                                          timeout=timedelta(seconds=10))
+            self.browser.find_element('//a[text()="Download Business Case PDF"]').click()
+            while True:
+                if os.path.exists(f"{OUTPUT}/{self.agency_table['UII'][index]}.pdf"):
+                    break
+                else:
+                    pass
+
+    def match_pdfs(self, pdf_index):
         """
         This function performs the bonus task, open PDF and start matching the UII Code
         and Investment Title.
 
-        :return: None
         """
-        pdf_file = f'output/{uii}.pdf'
-        pdf_name = self.pdf.get_text_from_pdf(pdf_file, pages=1)[1].split("Name of this Investment:")[1].split(".")[0]
-        pdf_uii_code = self.pdf.get_text_from_pdf(pdf_file, pages=1)[1].split("Unique Investment Identifier (UII)")[1].split("Section")[0]
+        for index in pdf_index:
+            unique_id = f"{self.agency_table['UII'][index]}"
+            name = f"{self.agency_table['Investment Title'][index]}"
+            pdf_file = f"{OUTPUT}/{self.agency_table['UII'][index]}.pdf"
+            pdf_name = self.pdf.get_text_from_pdf(pdf_file, pages=1)[1].split(
+                "Name of this Investment:")[1].split(".")[0]
+            pdf_uii_code = self.pdf.get_text_from_pdf(pdf_file, pages=1)[1].split(
+                "Unique Investment Identifier (UII)")[1].split("Section")[0]
+            if unique_id in pdf_uii_code and name in pdf_name:
+                self.agency_table["PDF"].append("matched")
+            else:
+                self.agency_table["PDF"].append("not matched")
 
-        if uii in pdf_uii_code and investment in pdf_name:
-            return True
-        return False
+    def get_headers(self):
+        headers = []
+        raw_headers = self.browser.find_elements(f"//div[@id='investments-table-object_wrapper']//table/thead/tr[2]/th")
+        for head in raw_headers:
+            headers.append(head.text)
+        return headers
 
-    def close_browser(self):
-        """
-        This Function close the browser when task is completed
-        :return: None
-
-        """
-        self.browser.close_browser()
+    def write_data_to_workbook(self):
+        self.files.create_workbook(f"{OUTPUT}/agencies.xlsx")
+        self.files.remove_worksheet("Sheet")
+        self.files.create_worksheet("AGENCIES")
+        self.files.append_rows_to_worksheet(self.agencies_data, header=True)
+        self.files.create_worksheet("UII TABLE")
+        self.files.append_rows_to_worksheet(self.agency_table, header=True)
+        self.files.save_workbook()
+        self.files.close_workbook()
--- task.py
+++ task.py
@@ -3,6 +3,6 @@ from config import AGENCY_NAME
 
 if __name__ == "__main__":
     it_dashboard = DashBoard()
-    it_dashboard.dive_in_agencies()
-    it_dashboard.agency_table_scrape(AGENCY_NAME)
-    it_dashboard.close_browser()
+    it_dashboard.dive_in()
+    it_dashboard.scrap_agencies()
+    it_dashboard.scrap_agency_table(AGENCY_NAME)
